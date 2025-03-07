bdb8f57495d904c5287085f9e508e8052c2f3e13
ShadowCube264 <68464043+ShadowCube264@users.noreply.github.com>
2025-03-07 14:37:01 +0000

    Initial Commit (just testing the library for now!)

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var midiFile = MidiFile.Read("../../Music/test2.mid");
+        foreach (var track in midiFile.GetTrackChunks())
+        {
+            Console.WriteLine("Track!");
+            Console.WriteLine(track.ChunkId);
+            Console.WriteLine(track.GetDuration(TimeSpanType.Metric, midiFile.GetTempoMap()));
+            foreach (var note in track.GetNotes())
+            {
+                Console.WriteLine("Note!");
+                Console.WriteLine(note.NoteNumber);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+just trying things and working with midi ;)
